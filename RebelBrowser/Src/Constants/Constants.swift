db5de0fd1376f745eace6002116df9a9d83392be
evilisn_jiang <evilisn_jiang@trendmicro.com.cn>
2017-05-11 17:19:30 +0800

    added constants

---
+++ RebelBrowser/Src/Constants/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  RebelBrowser
+//
+//  Created by Evilisn on 11/May/2017.
+//  Copyright © 2017 EvilisnJ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Config {
+    static let baseURL = URL(string: "http://www.example.org/")!;
+    static let splineReticulatorName = "foobar";
+}
+
+struct Color {
+    static let primaryColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let secondaryColor = UIColor.lightGray;
+}
